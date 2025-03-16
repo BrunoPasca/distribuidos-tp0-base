9b92fd0cb8dd325734b9ef964290e5b6a9fd043b
BrunoPasca <42937086+BrunoPasca@users.noreply.github.com>
2025-03-16 20:46:01 -0300

    fixes order of services

--- generar-compose.sh
+++ generar-compose.sh
@@ -30,6 +30,14 @@ services:
     networks:
       - testing_net
 
+  netcat:
+    container_name: netcat
+    image: alpine:latest
+    entrypoint: sh -c "apk add --no-cache netcat-openbsd && sleep infinity"
+    networks:
+      - testing_net
+    depends_on:
+      - server
 EOF
 
 until [ "$client_id" -gt "$num_clients" ]; do
@@ -52,15 +60,6 @@ EOF
 done
 
 cat <<EOF >> "$filename"
-    netcat:
-        container_name: netcat
-        image: alpine:latest
-        entrypoint: sh -c "apk add --no-cache netcat-openbsd && sleep infinity"
-        networks:
-          - testing_net
-        depends_on:
-          - server
-
 networks:
   testing_net:
     ipam:
